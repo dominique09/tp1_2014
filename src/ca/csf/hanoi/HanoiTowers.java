5565f0ec698f6eb69c28f85ecd950d739c4969c9
BigSmashy <geremy.sorlini.protic@gmail.com>
2014-09-11 11:33:55 -0400

    HanoiTowers class
    The class has been fully coded.

--- src/ca/csf/hanoi/HanoiTowers.java
+++ src/ca/csf/hanoi/HanoiTowers.java
@@ -33,6 +33,37 @@ public class HanoiTowers {
 		towers[towerPosition-1].removeDisk();
 	}
 	
+	public boolean canPickUp (int towerPosition){
+		if (heldDisk == null && towers[towerPosition-1].disks.isEmpty()){ // Not currently holding a disk, selected tower has disks.
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
 	
+	public void dropDisk (int towerPosition){
+		towers[towerPosition-1].addDisk(heldDisk); // Adds a disk to the tower, player is no longer holding a disk.
+		heldDisk = null;
+	}
+	
+	public boolean canDrop (int towerPosition){
+		if (heldDisk != null && heldDisk.size < towers[towerPosition-1].getDiskOnTop().size){ // Currently holding a disk. Held disk is smaller than the disk on top of target tower.
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isFinished () { // "Did we win?"
+		if (towers[2].getSize() == nbrOfDisks){ // If the right tower has all the disks in the game.
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 }

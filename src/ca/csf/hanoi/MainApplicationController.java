ed21254fd208379a22d0f7e857b0e2aa35175aff
Dominionized <dominique.begin.0@gmail.com>
2014-09-11 11:40:37 -0400

    Initial ui

--- src/ca/csf/hanoi/MainApplicationController.java
+++ src/ca/csf/hanoi/MainApplicationController.java
@@ -1,5 +1,14 @@
 package ca.csf.hanoi;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
 public class MainApplicationController {
+
+	@FXML Button newGameButton;
+	@FXML HBox bottom;
+
+	@FXML public void startNewGame() {}
 
 }

b524be155091473912983a59915778900bdd9ace
Dominique Septembre <dominique.septembre@gmail.com>
2014-09-10 20:47:26 -0400

    LinkedListStack + LinkedListStackNode Complete

--- src/ca/csf/stack/LinkedListStack.java
+++ src/ca/csf/stack/LinkedListStack.java
@@ -2,42 +2,71 @@ package ca.csf.stack;
 
 public class LinkedListStack implements Stack {
 	private int size;
-	private LinkedListStackNode lastStackcNode;
+	private LinkedListStackNode lastStackNode;
 	
 	public LinkedListStack(){
-		
-	}
-	
-	public LinkedListStack(Stack stack){
-		
+		size = 0;
+		lastStackNode = null;
 	}
 	
 	public Object pop(){
-		return new Object();
+		if(!isEmpty()){
+			LinkedListStackNode nodeToRemove = lastStackNode;
+			
+			size--;
+			
+			if(size > 0){
+				lastStackNode = nodeToRemove.getPreviousListStackNode();
+			} else {
+				lastStackNode = null;
+			}	
+			
+			return nodeToRemove.getObject();
+		} else {
+			return null;
+		}
+		
 	}
 	
-	public void push(Object object){
-		
+	public void push(Object object){;
+		lastStackNode = new LinkedListStackNode(object, lastStackNode);
+		size++;
 	}
 	
 	public Object peek(){
-		return new Object();
+		if(!isEmpty()){
+		return lastStackNode.getObject();
+		} else {
+			return null;
+		}
 	}
 	
 	public Object getAt(int position){
-		return new Object();
+		LinkedListStackNode node = lastStackNode;
+		for(int i = 0; i < position; ++i){
+			node = node.getPreviousListStackNode();
+		}
+		
+		return node.getObject();
 	}
 	
 	public void clear(){
-		
+		int listStackSize = size;
+		for(int i = 0; i < listStackSize; ++i){
+			pop();
+		}
 	}
 	
 	public boolean isEmpty(){
-		return true;
+		if (size == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public int getSize(){
-		return 0;
+		return size;
 	}
 	
 	public String toString(){

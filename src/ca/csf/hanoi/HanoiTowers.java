30e728fea49071e1f5d6970ee8fbb283672fc940
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 13:24:29 -0400

    Refactored the condition that checks if the number of disks is greater than 0

--- src/ca/csf/hanoi/HanoiTowers.java
+++ src/ca/csf/hanoi/HanoiTowers.java
@@ -5,22 +5,15 @@ public class HanoiTowers {
 	private int nbDisks; // The number of disks set for this game.
 	// ***UNNEEDED*** public Disk heldDisk; // A reference to the disk that is currently being held. (Used when picking up a disk from a tower)
 	
-	public HanoiTowers (int numberOfDisks) { // When first created, uses the default 3-disk configuration.
+	public HanoiTowers (int numberOfDisks) throws Exception { // When first created, uses the default 3-disk configuration.
+		
+		if (numberOfDisks > 0) throw new Exception("Number of disks needs to be at least 1");
 		towers = new Tower[3];
 		nbDisks = numberOfDisks;
 		
 		for (int i = numberOfDisks; i > 0; i--){
 			towers[0].addDisk(new Disk(i));
 		}
-	}
-	public boolean canAcceptNewGame (int numberOfDisks){
-		if (numberOfDisks > 0){ // This ensures that the requested amount of disks for a game is always a positive number.
-			return true;
-		}
-		else {
-			return false;
-		}
-		
 	}
 	
 	public boolean switchDisk (int initialTower, int finalTower) {

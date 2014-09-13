37543ec5455c72786568e346cb32f88fce89b78d
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 13:20:53 -0400

    Corrected typo in comment

--- src/ca/csf/hanoi/HanoiTowers.java
+++ src/ca/csf/hanoi/HanoiTowers.java
@@ -3,20 +3,16 @@ package ca.csf.hanoi;
 public class HanoiTowers {
 	public Tower[] towers; // The three towers used in the game. Towers are listed from left to right.
 	private int nbDisks; // The number of disks set for this game.
-	// ***UNNNEEDED*** public Disk heldDisk; // A reference to the disk that is currently being held. (Used when picking up a disk from a tower)
+	// ***UNNEEDED*** public Disk heldDisk; // A reference to the disk that is currently being held. (Used when picking up a disk from a tower)
 	
 	public HanoiTowers (int numberOfDisks) { // When first created, uses the default 3-disk configuration.
 		towers = new Tower[3];
-		newGame(numberOfDisks);
-	}
-	
-	public void newGame (int numberOfDisks) { // Begins a new game
 		nbDisks = numberOfDisks;
+		
 		for (int i = numberOfDisks; i > 0; i--){
 			towers[0].addDisk(new Disk(i));
 		}
 	}
-	
 	public boolean canAcceptNewGame (int numberOfDisks){
 		if (numberOfDisks > 0){ // This ensures that the requested amount of disks for a game is always a positive number.
 			return true;

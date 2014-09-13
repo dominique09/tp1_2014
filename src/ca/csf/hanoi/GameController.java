a0d8465f335dabcd64840bfe8126ca3898fa6a69
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 12:34:19 -0400

    Refactored the GameController not to create towers manually

--- src/ca/csf/hanoi/GameController.java
+++ src/ca/csf/hanoi/GameController.java
@@ -5,26 +5,9 @@ import com.sun.javafx.geom.Rectangle;
 import ca.csf.stack.LinkedListStack;
 
 public class GameController {
+	private HanoiTowers hanoiTowers;
 	
-	private Tower tower1;
-	private Tower tower2;
-	private Tower tower3;
-	
-	
-	
-	public void setupTowers(int numberOfDisks){
-		tower1 = new Tower(new LinkedListStack());
-		for (int i = 0; i < numberOfDisks; i++) {
-			tower1.addDisk(new Disk(1));
-		}
-		tower2 = new Tower(new LinkedListStack());
-		tower3 = new Tower(new LinkedListStack());
-		
-		for ()
+	public GameController() {
+		hanoiTowers = new HanoiTowers();
 	}
-	
-	public void updateTowers(Tower tower1, Tower tower2, Tower tower3){
-		
-	}
-	
 }

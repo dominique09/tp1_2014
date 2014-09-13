f602a6b8e98e98d45d4d6269f711a93ae8acf7b7
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 17:13:36 -0400

    Various things

--- src/ca/csf/hanoi/GameController.java
+++ src/ca/csf/hanoi/GameController.java
@@ -1,16 +1,15 @@
 package ca.csf.hanoi;
 
 import com.sun.javafx.geom.Rectangle;
-
 import ca.csf.stack.LinkedListStack;
 
 public class GameController {
-	private HanoiTowers hanoiTowers;
+	private HanoiTowers hanoiTowersGame;
 	
 	public GameController() {
 		try {
-			hanoiTowers = new HanoiTowers();
-			hanoiTowers.newGame(3); //TODO Don't hardcode like that.
+			hanoiTowersGame = new HanoiTowers();
+			hanoiTowersGame.newGame(3); //TODO Don't hardcode like that.
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

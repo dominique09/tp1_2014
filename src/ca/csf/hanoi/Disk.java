a98041e1664e901ba855936859e56d5fee8bdd72
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 13:30:44 -0400

    Added default constructor for Disk Default size is 1.

--- src/ca/csf/hanoi/Disk.java
+++ src/ca/csf/hanoi/Disk.java
@@ -2,9 +2,14 @@ package ca.csf.hanoi;
 
 public class Disk {
 	private final int size;
+	private static final int DEFAULT_SIZE = 1;
 	
 	public Disk(int size){
 		this.size = size;
+	}
+
+	public Disk() {
+		this.size = DEFAULT_SIZE;
 	}
 
 	public int getSize() {

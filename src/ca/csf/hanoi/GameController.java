3fbbb7de99231c1ed6783d1bb090572ced583b08
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 11:47:17 -0400

    Initial work on tower initialization

---
+++ src/ca/csf/hanoi/GameController.java
@@ -0,0 +1,20 @@
+package ca.csf.hanoi;
+
+public class GameController {
+	
+	private Tower tower1;
+	private Tower tower2;
+	private Tower tower3;
+	
+	public void setupTowers(int numberOfDisks){
+		tower1 = new Tower();
+		for (int i = 0; i < numberOfDisks; i++) {
+			tower1.push(new )
+		}
+	}
+	
+	public void updateTowers(){
+		
+	}
+	
+}
--- src/ca/csf/hanoi/MenuController.java
+++ src/ca/csf/hanoi/MenuController.java
@@ -12,4 +12,5 @@ public class MenuController {
 	@FXML public void startNewGame() {
 	}
 
+
 }

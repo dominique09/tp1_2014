ebb2c930215ab9f96636b22b7fb233d659d143a4
Dominionized <dominique.begin.0@gmail.com>
2014-09-13 13:54:25 -0400

    Fixed my error Cause i was a fucking idiot

--- src/ca/csf/hanoi/HanoiTowers.java
+++ src/ca/csf/hanoi/HanoiTowers.java
@@ -2,27 +2,34 @@ package ca.csf.hanoi;
 
 public class HanoiTowers {
 	public Tower[] towers; // The three towers used in the game. Towers are listed from left to right.
-	private int nbDisks; // The number of disks set for this game.
-	// ***UNNEEDED*** public Disk heldDisk; // A reference to the disk that is currently being held. (Used when picking up a disk from a tower)
+	private int nbrOfDisks; // The number of disks set for this game.
+	public Disk heldDisk; // The disk that is currently being held. (Used when picking up a disk from a tower)
 	
-	public HanoiTowers (int numberOfDisks) throws Exception { // When first created, uses the default 3-disk configuration.
-		
-		if (numberOfDisks < 1) throw new Exception("Number of disks needs to be at least 1");
+	public HanoiTowers () { // When first created, uses the default 3-disk configuration.
 		towers = new Tower[3];
-		nbDisks = numberOfDisks;
-		
+	}
+	
+	public void newGame (int numberOfDisks) { // Begins a new game
+		nbrOfDisks = numberOfDisks;
+		heldDisk = null;
 		for (int i = numberOfDisks; i > 0; i--){
 			towers[0].addDisk(new Disk(i));
 		}
 	}
 	
-	public boolean switchDisk (int initialTower, int finalTower) {
-		if (towers[initialTower-1].getDiskOnTop().getSize() > towers[finalTower].getDiskOnTop().getSize()){
-			towers[finalTower].addDisk(towers[initialTower].getDiskOnTop());
-			towers[initialTower].removeDisk();
+	public boolean canAcceptNewGame (int numberOfDisks){
+		if (numberOfDisks > 0){ // This ensures that the requested amount of disks for a game is always a positive number.
 			return true;
 		}
-		return false;
+		else {
+			return false;
+		}
+		
+	}
+	
+	public void pickUpDisk (int towerPosition){
+		heldDisk = towers[towerPosition-1].getDiskOnTop();
+		towers[towerPosition-1].removeDisk();
 	}
 	
 	public boolean canPickUp (int towerPosition){

b71bfff707cba168855b8f29caba418aaf41336e
Dominionized <dominique.begin.0@gmail.com>
2014-09-15 13:49:35 -0400

    Added schnitz (started adding rectangles dynamically)

--- src/ca/csf/hanoi/GameController.java
+++ src/ca/csf/hanoi/GameController.java
@@ -1,17 +1,35 @@
 package ca.csf.hanoi;
 
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
 import com.sun.javafx.geom.Rectangle;
+
 import ca.csf.stack.LinkedListStack;
 
 public class GameController {
 	private HanoiTowers hanoiTowersGame;
+	private GridPane mainGridPane;
+	
+	private static final int DISK_HEIGHT = 25;
+	private static final int TOWERS_ROW = 2;
 	
 	public GameController() {
 		try {
 			hanoiTowersGame = new HanoiTowers();
 			hanoiTowersGame.newGame(3); //TODO Don't hardcode like that.
+			for (Tower tower : hanoiTowersGame.towers) { // Pour chaque tour
+				for (int i = 0; i < tower.getSize(); i++){ // ... et pour chaque disque
+					Rectangle rectangle = new Rectangle(24,24);
+					mainGridPane.add(rectangle, 0, TOWERS_ROW);
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void updateRectangles(){
+		
 	}
 }

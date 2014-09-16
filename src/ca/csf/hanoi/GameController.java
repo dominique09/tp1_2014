f70cb9d0a3eabe3b32dbded43e7be8107d44a137
Dominionized <dominique.begin.0@gmail.com>
2014-09-16 10:12:48 -0400

    Added shnotz

--- src/ca/csf/hanoi/GameController.java
+++ src/ca/csf/hanoi/GameController.java
@@ -2,9 +2,9 @@ package ca.csf.hanoi;
 
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
-
-import com.sun.javafx.geom.Rectangle;
-
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import ca.csf.stack.LinkedListStack;
 
 public class GameController {
@@ -12,7 +12,7 @@ public class GameController {
 	private GridPane mainGridPane;
 	
 	private static final int DISK_HEIGHT = 25;
-	private static final int TOWERS_ROW = 2;
+	private static final int TOWERS_ROW = 1;
 	
 	public GameController() {
 		try {
@@ -20,8 +20,9 @@ public class GameController {
 			hanoiTowersGame.newGame(3); //TODO Don't hardcode like that.
 			for (Tower tower : hanoiTowersGame.towers) { // Pour chaque tour
 				for (int i = 0; i < tower.getSize(); i++){ // ... et pour chaque disque
-					Rectangle rectangle = new Rectangle(24,24);
-					mainGridPane.add(rectangle, 0, TOWERS_ROW);
+					Rectangle rectangle = new Rectangle(24,24, 30, 30);
+					rectangle.setFill(Color.PINK);
+					mainGridPane.add(rectangle, 0, 1);
 				}
 			}
 		} catch (Exception e) {

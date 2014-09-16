e38df795b01758f28388c435a9ad26fa4e8431fd
BigSmashy <geremy.sorlini.protic@gmail.com>
2014-09-16 10:13:07 -0400

    Created test

---
+++ test/ca/csf/hanoi/HanoiTowersTest.java
@@ -0,0 +1,36 @@
+package ca.csf.hanoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HanoiTowersTest {
+	
+	private HanoiTowers hanoiTowers;
+
+	@Before
+	public void setUp() throws Exception {
+		hanoiTowers = new HanoiTowers();
+	}
+
+	@Test
+	public void forHanoiTowers_whenTryingNewGameWithZeroDisks_cantAcceptNewGame() {
+		// I have no idea
+	}
+	
+	@Test
+	public void forHanoiTowers_whenPickingUpSingleDisk_heldDiskSizeIsOne() {
+		try {
+			hanoiTowers.newGame(1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		hanoiTowers.pickUpDisk(1);
+		
+	}
+	
+
+}

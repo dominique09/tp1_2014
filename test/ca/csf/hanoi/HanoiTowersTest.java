5f4dbbd1e84554e5fd05efddb3f66ae847a45e39
BigSmashy <geremy.sorlini.protic@gmail.com>
2014-09-16 11:07:27 -0400

    successfully created a test case where the game is solved, and the game confirms it.

--- test/ca/csf/hanoi/HanoiTowersTest.java
+++ test/ca/csf/hanoi/HanoiTowersTest.java
@@ -34,5 +34,52 @@ public class HanoiTowersTest {
 		
 	}
 	
+	@Test
+	public void forHanoiTowers_whenPickingUpSingleDiskDroppingItElsewhereThenPickingUpSecondDisk_heldDiskSizeIsTwo() {
+		try {
+			hanoiTowers.newGame(3);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		hanoiTowers.pickUpDisk(1);
+		
+		hanoiTowers.dropDisk(3);
+		
+		hanoiTowers.pickUpDisk(1);
+		
+		assertTrue(hanoiTowers.heldDisk.getSize() == 2);
+		
+	}
+	
+	@Test
+	public void forHanoiTowers_whenDoingClassicThreeDiskGameAndSolvingIt_isFinishedReturnsTrue() {
+		try {
+			hanoiTowers.newGame(3);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		hanoiTowers.pickUpDisk(1);// 123 0 0 (Memory aid for positions)
+		hanoiTowers.dropDisk(3); // 23 0 1
+		hanoiTowers.pickUpDisk(1);
+		hanoiTowers.dropDisk(2); // 3 2 1
+		hanoiTowers.pickUpDisk(3); 
+		hanoiTowers.dropDisk(2); // 3 12 0
+		hanoiTowers.pickUpDisk(1);
+		hanoiTowers.dropDisk(3); // 0 12 3
+		hanoiTowers.pickUpDisk(2);
+		hanoiTowers.dropDisk(1); // 1 2 3
+		hanoiTowers.pickUpDisk(2);
+		hanoiTowers.dropDisk(3); // 1 0 23
+		hanoiTowers.pickUpDisk(1);
+		hanoiTowers.dropDisk(3); // 0 0 123
+		
+		assertTrue(hanoiTowers.isFinished());
+		
+	}
+	
 
 }
